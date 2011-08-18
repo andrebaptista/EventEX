1f4554accdff874dcf1fcaa85976af2de2535de3
André Baptista <andrepb.wd@gmail.com>
2011-08-18 09:08:47 -0300

    All you base are belong us.

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import	staticfiles_urlpatterns
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'eventex.views.home', name='home'),
+    # url(r'^eventex/', include('eventex.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'core.views.home', name='home')
+)
